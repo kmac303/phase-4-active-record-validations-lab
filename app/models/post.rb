8d86683a9b806d0f7267dd400a8ceb1015c17ee8
support <support@kmcintosh-6782.remax.local>
2023-05-23 18:17:55 -0600

    commit tree 709a19d4f88a4e423eea8ad3e951dc3a91ab68e9

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ("Fiction", "Non-Fiction")}
+    validate :clickbait?
+
+    CLICKBAIT_PATTERNS = [
+        "Won't Believe",
+        "Secret",
+        "Top #{integer}",
+        "Guess"
+      ]
+    
+      def clickbait?
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+          errors.add(:title, "must be clickbait")
+        end
+      end
+
 end
